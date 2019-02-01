7760b3b144f20269ee9b95d612e89733219b6b8b
Nickolas Burr <nickolasburr@gmail.com>
2019-02-01 12:59:15 -0500

    Clean up app.js

--- app/themes/nickolasburr/scripts/app.js
+++ app/themes/nickolasburr/scripts/app.js
@@ -1,27 +1,23 @@
+/**
+ * app.js: Main application JS file.
+ */
 (function ($) {
-	$(document).ready(onReady);
-	// _DOMContentLoaded_ event handler
-	function onReady () {
-		// add _click_ handlers to each section arrow
+	var App = {};
+
+	// DOMContentLoaded event handler.
+	App.onReady = function () {
 		$('.main .cv .wrapper .row i').each(function () {
-			$(this).click(onToggleSection);
-		});
-		// add _click_ handlers to each review button
-		$('.main .special i').each(function () {
-			// $(this).click(onToggleReview);
+			$(this).click(App.onToggleSection);
 		});
 	}
-	/**
-	*
-	* sections (blocks) event handlers
-	*
-	*/
-	// toggle section _click_ event handler
-	function onToggleSection (clickEvent) {
+
+	// Toggle section click event handler.
+	App.onToggleSection = function (clickEvent) {
 		var buttonClass, blockClass;
 		var block = $(this).closest('.wrapper').find('.block');
 		var button = this;
 		var currentClass = block.attr('class');
+
 		if (currentClass.indexOf('hidden') > -1) {
 			blockClass  = 'block visible';
 			buttonClass = 'fa fa-chevron-up open';
@@ -31,7 +27,8 @@
 		}
 		$(block).attr('class', blockClass);
 		$(this).attr('class', buttonClass);
-		// update the other blocks and buttons
+
+		// Update the other blocks and buttons
 		$('.main .cv .wrapper .row i').each(function (index, element) {
 			if ($(element).attr('class') === $(button).attr('class') && element !== button) {
 				$(element).attr('class', 'fa fa-chevron-down closed');
@@ -43,32 +40,6 @@
 			}
 		});
 	}
-	/**
-	*
-	* reviews event handlers
-	*
-	*/
-	// toggle review _click_ event handler
-	function onToggleReview (clickEvent) {
-		var contentHolder = $(this).closest('.book').find('.review');
-		var classAttr = contentHolder.attr('class');
-		if (classAttr.indexOf('hidden') > -1) {
-			contentHolder.attr('class', contentHolder.attr('class').split('hidden')[0].trim() + ' visible');
-			$(this).attr('class', 'fa fa-chevron-up open');
-			return closeOpenReviews(this);
-		}
-		contentHolder.attr('class', contentHolder.attr('class').split('visible')[0].trim() + ' hidden');
-		$(this).attr('class', 'fa fa-chevron-down closed');
-		return closeOpenReviews(this);
-	}
-	// close open reviews and limit one open review at a time
-	function closeOpenReviews (activeElement) {
-		$('.main .special i').each(function (index, element) {
-			if ($(element).attr('class') === $(activeElement).attr('class') && element !== activeElement) {
-				var contentHolder = $(element).closest('.book').find('.review');
-				contentHolder.attr('class', contentHolder.attr('class').split('visible').length > 1 ? contentHolder.attr('class').split('visible')[0].trim() + ' hidden' : contentHolder.attr('class'));
-				$(element).attr('class', 'fa fa-chevron-down closed');
-			}
-		});
-	}
+
+	$(document).ready(onReady);
 }).call(this, jQuery);
